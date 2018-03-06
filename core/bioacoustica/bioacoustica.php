26a3645ee585137994cc543a0a25c49ed4ee4dea
Ed Baker <edwbaker@gmail.com>
2018-03-06 14:04:26 +0000

    BioAcoustica depends on s3cmd.

--- core/bioacoustica/bioacoustica.php
+++ core/bioacoustica/bioacoustica.php
@@ -7,7 +7,17 @@ function bioacoustica_init() {
       "required" => "required",
       "missing text" => "BioAcoustica requires R.",
       "version flag" => "--version"
+    ),
+    "s3cmd" => array(
+      "type" => "cmd",
+      "required" => "required",
+      "missing text" => "BioAcoustica requires s3cmd to access data.",
+      "version flag" => "--version"
     )
   );
   return($init);
+}
+
+function bioacoustica_prepare() {
+
 }
